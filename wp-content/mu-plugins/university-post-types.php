60232fbbaeb2fb9450fdacddd2ac8c132f8f18a9
codeforhome <83600086+codeforhome@users.noreply.github.com>
2022-01-05 15:11:36 +0800

    section 8 done

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_post_type(){
+    //Event Post Type
     register_post_type('event',array(
         'public' => true,
         'has_archive' => true,
@@ -18,6 +19,26 @@ function university_post_type(){
         // This is where we add taxonomies to our CPT
         'taxonomies'          => array( 'category' ),
         'menu_icon' => 'dashicons-calendar',
+    ));
+
+    // Program Post Type
+    register_post_type('program',array(
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'program'),
+        'supports' => array('title','editor'),
+//        'supports' => array('title','editor','excerpt', 'custom-fields'),
+        'show_in_rest' => true, //use new blog editor
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Program',
+            'singular_name' => 'Program',
+        ),
+        // This is where we add taxonomies to our CPT
+//        'taxonomies'          => array( 'category' ),
+        'menu_icon' => 'dashicons-awards',
     ));
 }
 
