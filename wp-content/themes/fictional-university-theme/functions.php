89301fa439b856679f40f7311db79fe64ff98034
codeforhome <83600086+codeforhome@users.noreply.github.com>
2022-01-05 16:52:13 +0800

    section 9  end

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -22,6 +22,7 @@ function university_feature(){
     add_theme_support('post-thumbnails');
     add_image_size('professorLandscape',400,260,true);
     add_image_size('professorPortrait',480,650,true);
+    add_image_size('pageBanner',1500,350,true);
 
 }
 
