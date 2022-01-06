202bebb49d15d91917d6609266ea45bd9d45eadf
codeforhome <83600086+codeforhome@users.noreply.github.com>
2022-01-06 23:58:02 +0800

    open and close search overlay

--- wp-content/themes/fictional-university-theme/build/index.asset.php
+++ wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '647e05be43604b4550a30c37fac95e17');
+<?php return array('dependencies' => array('jquery'), 'version' => '06e33168879754ee994fced29ec755a4');
--- wp-content/themes/fictional-university-theme/src/index.js
+++ wp-content/themes/fictional-university-theme/src/index.js
@@ -4,9 +4,12 @@ import "../css/style.scss"
 import MobileMenu from "./modules/MobileMenu"
 import HeroSlider from "./modules/HeroSlider"
 import GoogleMap from "./modules/GoogleMap"
+import Search from "./modules/search"
 
 // Instantiate a new object using our modules/classes
 const mobileMenu = new MobileMenu()
 const heroSlider = new HeroSlider()
 const googleMap = new GoogleMap()
+const search = new Search()
+
 
